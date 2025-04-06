79995f84d8452996ff2072f39d7cc09db1c8fd16
pawan2505 <pawanaktu@gmail.com>
2025-04-06 10:09:52 +0530

    array

---
+++ C Programming/Array/code.c
@@ -0,0 +1,68 @@
+// #include<stdio.h>
+// int main(){
+//     int n = 10;
+
+//     int arr[n];
+
+//     arr[0] = 10;
+//     arr[1] = 20;
+//     arr[2] = 30;
+//     arr[3] = 33;
+//     arr[4] = 40;
+//     arr[5] = 50;
+//     arr[6] = 60;
+//     arr[7] = 70;
+//     arr[8] = 80;
+//     arr[9] = 90;
+
+//     printf("%d\n", arr[0]);
+//     printf("%d\n", arr[1]);
+//     printf("%d\n", arr[2]);
+//     printf("%d\n", arr[3]);
+//     printf("%d\n", arr[4]);
+//     printf("%d\n", arr[5]);
+//     printf("%d\n", arr[6]);
+//     printf("%d\n", arr[7]);
+//     printf("%d\n", arr[8]);
+//     printf("%d\n", arr[9]);
+
+// }
+
+// #include <stdio.h>
+// int main()
+// {
+//     int n = 10;
+
+//     int arr[n];
+
+//     arr[0] = 10;
+//     arr[1] = 20;
+//     arr[2] = 30;
+//     arr[3] = 33;
+//     arr[4] = 40;
+//     arr[5] = 50;
+//     arr[6] = 60;
+//     arr[7] = 70;
+//     arr[8] = 80;
+//     arr[9] = 90;
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         printf("%d\n", arr[i]);
+//     }
+// }
+
+// #include <stdio.h>
+// int main()
+// {
+//     int arr[10] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+
+//     for (int i = 0; i < 10; i++)
+//     {
+//         printf("%d\n", arr[i]);
+//     }
+//     return 0;
+// }
+
+
+
